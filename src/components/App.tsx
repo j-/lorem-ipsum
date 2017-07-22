a38fbc44393e6cfee2ae6cf96ba6b336e4be9fc8
j- <j@skeoh.com>
2017-07-22 19:38:50 +1000

    Add GitHub ribbon

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import Ribbon from './Ribbon';
 import LoremIpsumButtons from './LoremIpsumButtons';
 
 const App = () => (
 	<div className="App">
+		<Ribbon />
 		<h1>Lorem ipsum</h1>
 		<LoremIpsumButtons max={5} units="words" />
 		<LoremIpsumButtons max={5} units="sentences" />
